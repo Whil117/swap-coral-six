8f80dce99e25b61934731567c6e7dfb6601faf14
Ivan García <miltonivan117@gmail.com>
2022-10-19 22:38:30 -0500

    REFACT

--- src/pages/public/favorites/index.tsx
+++ src/pages/public/favorites/index.tsx
@@ -5,19 +5,12 @@ import { css } from '@emotion/react';
 import useTimerDrag, { DragPagesAtom } from '@Hooks/useTimerDrag';
 import { IImage } from '@Types/index';
 import { Reorder } from 'framer-motion';
-import { atom, useAtom, useSetAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 import { NextPageFCProps } from 'next';
 import { useRouter } from 'next/router';
 import MY_FAVORITES_REDUCER_ATOM from '_jotai/favoritesSongs/reducer';
 import CONTROLS_PLAYER_WITH_REDUCER_ATOM from '_jotai/player/reducer';
 import { Item } from './ADF';
-
-const initialItems = atom([
-  '🍅 Tomato',
-  '🥒 Cucumber',
-  '🧀 Cheese',
-  '🥬 Lettuce'
-]);
 
 const MyFavorites: NextPageFCProps = () => {
   const [favorites, setFavorites] = useAtom(MY_FAVORITES_REDUCER_ATOM);
@@ -99,76 +92,6 @@ const MyFavorites: NextPageFCProps = () => {
             />
           ))}
         </Reorder.Group>
-        {/* <Reorder.Group axis="y" values={favorites} onReorder={setFavorites}>
-          {favorites?.map((item, index) => (
-            <Reorder.Item
-              key={item.track_number}
-              value={item}
-              style={{
-                color: 'transparent'
-              }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              dragListener={false}
-              dragControls={dragControls}
-              onDrag={() => {
-                setDrag(true);
-                setTimer(0);
-              }}
-            >
-              <AtomIcon
-                width="20px"
-                height="30px"
-                color="#f33131"
-                icon="https://storage.googleapis.com/cdn-bucket-ixulabs-platform/IXU-0001/icons/sideBar/reorder-icon.svg"
-                onPointerDown={(event) => {
-                  dragControls.start(event);
-                }}
-                customCSS={css`
-                  padding: 0 10px;
-                  cursor: pointer;
-                  align-self: center;
-                `}
-              />
-              <AtomTrack
-                type="dragable"
-                onPlay={() => {
-                  dispatch({
-                    type: 'SET_TRACK',
-                    payload: {
-                      currentTrack: {
-                        ...item,
-                        // artists: data?.albumById?.artists,
-                        images: item?.album?.images as IImage[],
-                        track_number: index + 1,
-                        album: item?.album,
-                        destination: {
-                          type: 'album',
-                          id: item?.album?.id as string
-                        }
-                      },
-                      context: favorites.map((item, index) => ({
-                        ...item,
-                        track_number: index + 1
-                      })),
-                      origin: router
-                    }
-                  });
-                }}
-                onFavorite={() => {
-                  setFavorites((prev) =>
-                    prev.filter((favorites) => favorites.id !== item.id)
-                  );
-                }}
-                album={{
-                  ...(item as ISong),
-                  track_number: index + 1
-                }}
-              />
-            </Reorder.Item>
-          ))}
-        </Reorder.Group> */}
       </AtomWrapper>
     </AtomWrapper>
   );
