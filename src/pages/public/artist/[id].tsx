a4c28294a25a530e50c350546ce7ab5e1e7ae251
Ivan García <miltonivan117@gmail.com>
2022-11-02 12:27:45 -0600

    albums BY ARTISTS

--- src/pages/public/artist/[id].tsx
+++ src/pages/public/artist/[id].tsx
@@ -3,13 +3,14 @@ import client from '@Apollo/client/notWSS';
 import { ARTISTBYID } from '@Apollo/client/query/artistById';
 import { LISTALBUMSBYARTISTID } from '@Apollo/client/query/listAlbums';
 import AtomBanner from '@Components/@atoms/AtomBanner';
-import AtomButton from '@Components/@atoms/AtomButton';
 import AtomCard from '@Components/@atoms/AtomCard';
 import AtomLoader from '@Components/@atoms/AtomLoader';
 import AtomSEO from '@Components/@atoms/AtomSeo';
 import AtomWrapper from '@Components/@atoms/Atomwrapper';
 import { css } from '@emotion/react';
-import { IQueryFilter } from '@Types/index';
+import { IAlbumType, IQueryFilter } from '@Types/index';
+import { useSetAtom } from 'jotai';
+import { ISBOTTOM_ATOM } from 'layout/public/VIEW';
 import { NextPageContext, NextPageFC } from 'next';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -23,7 +24,9 @@ const paginationInitial = {
 
 const PublicArtist: NextPageFC<{ id: string }> = (props) => {
   const [pagination, setPagination] = useState(paginationInitial);
+  const [ALBUMS, SETALBUMARTIST] = useState([] as IAlbumType[]);
   const router = useRouter();
+  const setBottomView = useSetAtom(ISBOTTOM_ATOM);
   const { data, loading } = useQuery<IQueryFilter<'artistById'>>(ARTISTBYID, {
     skip: !props.id,
     fetchPolicy: 'no-cache',
@@ -31,7 +34,7 @@ const PublicArtist: NextPageFC<{ id: string }> = (props) => {
       id: props?.id
     }
   });
-  const { data: ListAlbumsByArtist, loading: LoadingAlbums } = useQuery<
+  const { loading: LoadingAlbums, refetch } = useQuery<
     IQueryFilter<'listAlbums'>
   >(LISTALBUMSBYARTISTID, {
     skip: !props.id,
@@ -47,12 +50,28 @@ const PublicArtist: NextPageFC<{ id: string }> = (props) => {
       }
     },
     onCompleted: (data) => {
+      setBottomView({
+        callback: () => {
+          if (data?.listAlbums?.hasNextPage) {
+            refetch();
+            setPagination((prev) => ({
+              ...prev,
+              page: (data?.listAlbums?.page as number) + 1,
+              offset: (((data?.listAlbums?.page as number) + 1) as number) * 50
+            }));
+          }
+        }
+      });
       setPagination({
         total: data?.listAlbums?.total as number,
         limit: data?.listAlbums?.limit as number,
         offset: data?.listAlbums?.offset as number,
         page: pagination.page
       });
+      SETALBUMARTIST((prev) => [
+        ...prev,
+        ...((data?.listAlbums?.items as IAlbumType[]) ?? [])
+      ]);
     }
   });
 
@@ -66,89 +85,59 @@ const PublicArtist: NextPageFC<{ id: string }> = (props) => {
         description={data?.artistById?.name}
       />
       {loading ? (
-        <AtomLoader type="small" isLoading colorLoading="white" />
+        <AtomWrapper alignItems="center" justifyContent="center">
+          <AtomLoader type="small" isLoading colorLoading="white" />
+        </AtomWrapper>
       ) : (
         <AtomBanner type="artist" artist={data?.artistById} />
       )}
-      {LoadingAlbums ? (
-        <AtomLoader type="small" isLoading colorLoading="white" />
-      ) : (
+
+      <AtomWrapper
+        padding="45px 90px"
+        maxWidth="1440px"
+        flexDirection="column"
+        flexWrap="wrap"
+        customCSS={css`
+          display: flex;
+          gap: 10px;
+          @media (max-width: 980px) {
+            padding: 0px 30px;
+          }
+        `}
+      >
         <AtomWrapper
-          padding="45px 90px"
-          maxWidth="1440px"
-          flexDirection="column"
+          flexDirection="row"
           flexWrap="wrap"
+          gap="10px"
           customCSS={css`
-            display: flex;
-            gap: 10px;
             @media (max-width: 980px) {
               padding: 0px 30px;
             }
           `}
         >
-          <AtomWrapper
-            flexDirection="row"
-            flexWrap="wrap"
-            gap="10px"
-            customCSS={css`
-              @media (max-width: 980px) {
-                padding: 0px 30px;
-              }
-            `}
-          >
-            {ListAlbumsByArtist?.listAlbums?.items?.map((item) => (
-              <AtomCard
-                key={item?.id}
-                {...item}
-                image={item?.images?.[0]?.url as string}
-                type={item?.album_type}
-                onClick={() => {
-                  router.push({
-                    pathname: '/public/album/[id]',
-                    query: {
-                      id: item?.id
-                    }
-                  });
-                }}
-              />
-            ))}
-          </AtomWrapper>
-          <AtomWrapper flexDirection="row" justifyContent="space-between">
-            {ListAlbumsByArtist?.listAlbums?.hasPreviousPage && (
-              <AtomButton
-                onClick={() => {
-                  if (ListAlbumsByArtist?.listAlbums?.hasPreviousPage) {
-                    setPagination((prev) => ({
-                      ...prev,
-                      page: (ListAlbumsByArtist?.listAlbums?.page as number) - 1
-                    }));
+          {ALBUMS?.map((item) => (
+            <AtomCard
+              key={item?.id}
+              {...item}
+              image={item?.images?.[0]?.url as string}
+              type={item?.album_type}
+              onClick={() => {
+                router.push({
+                  pathname: '/public/album/[id]',
+                  query: {
+                    id: item?.id
                   }
-                }}
-              >
-                Previous Albums
-              </AtomButton>
-            )}
-            {ListAlbumsByArtist?.listAlbums?.hasNextPage && (
-              <AtomButton
-                onClick={() => {
-                  if (ListAlbumsByArtist?.listAlbums?.hasNextPage) {
-                    setPagination((prev) => ({
-                      ...prev,
-                      page:
-                        (ListAlbumsByArtist?.listAlbums?.page as number) + 1,
-                      offset:
-                        (((ListAlbumsByArtist?.listAlbums?.page as number) +
-                          1) as number) * 50
-                    }));
-                  }
-                }}
-              >
-                More Albums
-              </AtomButton>
-            )}
-          </AtomWrapper>
+                });
+              }}
+            />
+          ))}
         </AtomWrapper>
-      )}
+        <AtomWrapper alignItems="center" justifyContent="center">
+          {LoadingAlbums && (
+            <AtomLoader type="small" isLoading colorLoading="white" />
+          )}
+        </AtomWrapper>
+      </AtomWrapper>
     </AtomWrapper>
   );
 };
