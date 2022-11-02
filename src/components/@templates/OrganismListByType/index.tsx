d1c6a6ed25aa301c6c15ad86418d71ab444ef34f
Ivan García <miltonivan117@gmail.com>
2022-11-02 12:40:09 -0600

    sdalkfasdf

--- src/components/@templates/OrganismListByType/index.tsx
+++ src/components/@templates/OrganismListByType/index.tsx
@@ -1,29 +1,59 @@
 import { useQuery } from '@apollo/client';
 import { LISTBYTYPE } from '@Apollo/client/query/listByType';
-import AtomButton from '@Components/@atoms/AtomButton';
 import AtomCard from '@Components/@atoms/AtomCard';
 import AtomLoader from '@Components/@atoms/AtomLoader';
 import AtomSEO from '@Components/@atoms/AtomSeo';
 import { AtomText } from '@Components/@atoms/AtomText';
 import AtomWrapper from '@Components/@atoms/Atomwrapper';
 import { css } from '@emotion/react';
-import { IQueryFilter } from '@Types/index';
+import {
+  IAlbumType,
+  IArtist,
+  IlistPlaylistsBySlug,
+  IQueryFilter
+} from '@Types/index';
+import { atom, useAtom, useSetAtom } from 'jotai';
+import { ISBOTTOM_ATOM } from 'layout/public/VIEW';
 import { useRouter } from 'next/router';
 import { FC, useState } from 'react';
 type Props = {
   type: 'artists' | 'albums' | 'playlist';
 };
+type ATOM = {
+  [key: string]: IAlbumType[] | IArtist[] | IlistPlaylistsBySlug[];
+};
+
+const LISTBYTYPE_ATOM = atom<ATOM>({
+  albums: [],
+  artists: [],
+  playlist: []
+});
 
 const OrganismListByType: FC<Props> = (props) => {
   const [loading, setLoading] = useState(true);
+  const setBottomView = useSetAtom(ISBOTTOM_ATOM);
+  const [STATE, SETSTATE] = useAtom(LISTBYTYPE_ATOM);
   const router = useRouter();
-  const { data, refetch } = useQuery<IQueryFilter<'listByType'>>(LISTBYTYPE, {
+  const { refetch } = useQuery<IQueryFilter<'listByType'>>(LISTBYTYPE, {
     fetchPolicy: 'cache-and-network',
     variables: {
       type: [props.type],
       limit: 50
     },
-    onCompleted: () => {
+    onCompleted: (data) => {
+      setBottomView({
+        callback: () => {
+          refetch();
+          setLoading(true);
+        }
+      });
+      const AAAA = data?.listByType?.[props.type] as
+        | IAlbumType[]
+        | IArtist[]
+        | IlistPlaylistsBySlug[];
+      SETSTATE((prev) => ({
+        [props.type]: [...(prev?.[props.type] ?? []), ...(AAAA ?? [])]
+      }));
       setLoading(false);
     }
   });
@@ -47,54 +77,45 @@ const OrganismListByType: FC<Props> = (props) => {
           gap: 10px;
         `}
       >
-        {loading ? (
-          <AtomLoader type="small" isLoading colorLoading="white" />
-        ) : (
+        {[props.type]?.map((item) => (
           <>
-            <AtomButton
-              onClick={() => {
-                refetch();
-                setLoading(true);
-              }}
+            {!loading && (
+              <AtomText color="white" fontWeight="bold">
+                {item}
+              </AtomText>
+            )}
+            <AtomWrapper
+              width="100%"
+              flexDirection="row"
+              flexWrap="wrap"
+              customCSS={css`
+                display: flex;
+                gap: 10px;
+              `}
             >
-              Get More Data
-            </AtomButton>
-            {[props.type]?.map((item) => (
-              <>
-                <AtomText color="white" fontWeight="bold">
-                  {item}
-                </AtomText>
-                <AtomWrapper
-                  width="100%"
-                  flexDirection="row"
-                  flexWrap="wrap"
-                  customCSS={css`
-                    display: flex;
-                    gap: 10px;
-                  `}
-                >
-                  {data?.listByType?.[
-                    item as keyof typeof data.listByType
-                  ]?.map((props) => (
-                    <AtomCard
-                      key={props?.id}
-                      {...props}
-                      image={props?.images?.[0]?.url as string}
-                      onClick={() => {
-                        router.push({
-                          pathname: `/public/${props?.type}/[id]`,
-                          query: {
-                            id: props?.id
-                          }
-                        });
-                      }}
-                    />
-                  ))}
-                </AtomWrapper>
-              </>
-            ))}
+              {STATE?.[item as keyof typeof STATE]?.map((props) => (
+                <AtomCard
+                  key={props?.id}
+                  {...props}
+                  image={props?.images?.[0]?.url as string}
+                  onClick={() => {
+                    router.push({
+                      pathname: `/public/${props?.type}/[id]`,
+                      query: {
+                        id: props?.id
+                      }
+                    });
+                  }}
+                />
+              ))}
+            </AtomWrapper>
           </>
-        )}
+        ))}
+        <AtomWrapper alignItems="center" justifyContent="center">
+          {loading && (
+            <AtomLoader type="small" isLoading colorLoading="white" />
+          )}
+        </AtomWrapper>
       </AtomWrapper>
     </AtomWrapper>
   );
